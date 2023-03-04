707da961679d5c238b27c91d54749596b57d3f58
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2023-03-04 15:09:10 -0800

    Updated products padding and alignment

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,8 +3,8 @@ import ExtensionIcon from '@mui/icons-material/Extension';
 
 function Products(props) {
     return (
-        <div className='products col-md-8 mx-auto' id="products">
-            <div className="product">
+        <div className='products col-md-8 mx-auto d-flex align-items-center justify-content-evenly' id="products">
+            <div className="product align-items-center justify-content-evenly">
                 <ExtensionIcon style={{fontSize: '4rem'}} />
                 <div className="product-details">
                     <h3><b>Bio</b>nic <b>Fon</b>t <b>Plu</b>gin</h3>
@@ -12,7 +12,7 @@ function Products(props) {
                     <p><a href="/blog/5b21ca3eeb7f6fbccd471815">Learn More</a></p>
                 </div>
             </div>
-            <div className="product-download">
+            <div className="product-download align-items-center justify-content-evenly">
                 <h4>Available For: </h4>
                 <p>&bull; Chrome</p>
                 <button type="button" className="btn" disabled>Download</button>

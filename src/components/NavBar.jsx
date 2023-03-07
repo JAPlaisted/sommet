02d29d665fd6b321e798f25a879b0f3c34b1f654
Jonathan Plaisted <jonathan.plaisted@outlook.com>
2023-03-06 21:39:20 -0800

    Updated logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { HashLink } from 'react-router-hash-link';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
 
 function NavBar(props) {
     return (
     <nav className="navbar navbar-expand-lg navbar-dark">
-      <HashLink className="navbar-brand" to="/"><PersonAddIcon className='logo-icon' style={{fontSize: '2rem'}}/>Sommet Technologies</HashLink>
+      <HashLink className="navbar-brand" to="/"><img src="https://cdn.midjourney.com/966ff00a-2919-487b-be93-06350a802c1c/grid_0.png" alt="developer logo" className='logo' />Sommet Technologies</HashLink>
       <ul className="navbar-nav">
         <li className="nav-item"><HashLink className="nav-link" to="/blogs">Blog</HashLink></li>
         <li className="nav-item"><HashLink className="nav-link" to="/mission">Mission</HashLink></li>
